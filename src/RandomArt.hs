20b3ae73443a1e57978ba12d83dba17ba9a4e230
chris <cnhahn@ucsc.edu>
2019-04-24 12:45:43 -0700

    finished the eval and exprToString, thank the GODZZ! Why is this so hard and long.

--- src/RandomArt.hs
+++ src/RandomArt.hs
@@ -78,12 +78,20 @@ sampleExpr3 =
 
 exprToString :: Expr -> String
 exprToString VarX                 = "x"
-exprToString VarY                 = error "TBD:VarY"
-exprToString (Sine e)             = error "TBD:Sin"
-exprToString (Cosine e)           = error "TBD:Cos"
-exprToString (Average e1 e2)      = error "TBD:Avg"
-exprToString (Times e1 e2)        = error "TBD:Times"
-exprToString (Thresh e1 e2 e3 e4) = error "TBD:Thresh"
+--exprToString VarY                 = error "TBD:VarY"
+--exprToString (Sine e)             = error "TBD:Sin"
+--exprToString (Cosine e)           = error "TBD:Cos"
+--exprToString (Average e1 e2)      = error "TBD:Avg"
+--exprToString (Times e1 e2)        = error "TBD:Times"
+--exprToString (Thresh e1 e2 e3 e4) = error "TBD:Thresh"
+
+exprToString VarY                 = "y"
+exprToString (Sine e)             = "sin(pi*" ++ exprToString e ++ ")"
+exprToString (Cosine e)           = "cos(pi*" ++ exprToString e ++ ")"
+exprToString (Average e1 e2)      = "((" ++ exprToString e1 ++ "+" ++ exprToString e2 ++")/2)"
+exprToString (Times e1 e2)        = exprToString e1 ++ "*" ++ exprToString e2
+exprToString (Thresh e1 e2 e3 e4) = "(" ++ exprToString e1 ++ "<" ++ exprToString e2 ++ "?" ++ exprToString e3 ++ ":" ++ exprToString e4 ++ ")"
+
 
 --------------------------------------------------------------------------------
 -- | Evaluating Expressions at a given X, Y co-ordinate ------------------------
@@ -99,7 +107,25 @@ exprToString (Thresh e1 e2 e3 e4) = error "TBD:Thresh"
 -- 0.8090169943749475
 
 eval :: Double -> Double -> Expr -> Double
-eval x y e = error "TBD:eval"
+--eval x y e = error "TBD:eval"
+--eval x y e = if e == VarX then x
+--eval x y e = if e == VarY then y
+--eval x y e = if (e == (Sine e)) then sin(pi* (eval x y e))
+--eval x y e = if (e == (Cosine e)) then cos(pi* (eval x y e))
+--eval x y e = if (e == (Average e1 e2)) then ((eval x y e1) + (eval x y e2))/2
+--eval x y e = if (e == (Thresh e1 e2 e3 e4)) 
+--             then (if ((eval x y e1) < (eval x y e2)) then eval x y e3 else eval x y e4)
+--eval x y e
+--  | (e == VarX) = x
+--  | (e == VarY) = y 
+eval x y VarX = x
+eval x y VarY = y
+eval x y (Sine e) = sin(pi* (eval x y e))
+eval x y (Cosine e) = cos(pi* (eval x y e))
+eval x y (Average e1 e2) = ((eval x y e1) + (eval x y e2))/2
+eval x y (Times e1 e2) = ((eval x y e1) * (eval x y e2))
+eval x y (Thresh e1 e2 e3 e4) = if ((eval x y e1) < (eval x y e2)) then eval x y e3 else eval x y e4
+
 
 evalFn :: Double -> Double -> Expr -> Double
 evalFn x y e = assert (-1.0 <= rv && rv <= 1.0) rv
